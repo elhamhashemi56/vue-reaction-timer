860887b03c22318fa75d1a2f37be33989e742072
unknown <elhamhashemi56@yahoo.com>
2023-08-24 20:36:24 +0200

    Finishing Touches

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,13 +1,36 @@
 <template>
   <p>Reaction Time: {{ scoreProps }} ms</p>
+  <h2 class="rank">{{ rank }}</h2>
+  
 </template>
 
 <script>
 export default {
-props:["scoreProps"]
+props:["scoreProps"],
+data(){
+    return{
+        rank:null
+    }
+    
+},
+mounted(){
+    if(this.scoreProps < 250 ){
+        this.rank= "Sehr gut"
+
+    }else if(this.scoreProps < 400){
+        this.rank= "Gut"
+
+    }else{
+        this.rank= "Nicht gut"
+    }
+}
 }
 </script>
 
 <style>
-
+.rank {
+    font-size: 1.4em;
+    color: #0faf78;
+    font-weight: bold;
+}
 </style>
